7ef72d93902e975e8b01569708110c3e31030e28
elpaisa22 <sebastianecclesia@gmail.com>
2017-05-10 23:54:49 -0300

    Mejoras en los estilos de los crud

--- appl-parent/appl-frontend/appl-frontend-web/src/main/webapp/resources/js/jquery.gridselection.js
+++ appl-parent/appl-frontend/appl-frontend-web/src/main/webapp/resources/js/jquery.gridselection.js
@@ -1,15 +1,15 @@
 function gridBehavior() {
-	jQuery('#form-main\\:dataTable').find('tbody').find('tr').mousedown(function(){ return false; });
-	jQuery('#form-main\\:dataTable').find('tbody').find('tr').bind('dblclick', function(event){
+	jQuery('#dataTable').find('tbody').find('tr').mousedown(function(){ return false; });
+	jQuery('#dataTable').find('tbody').find('tr').bind('dblclick', function(event){
 			    dobleClick();
 			});
-	jQuery('#form-main\\:dataTable').find('tbody').find('tr').bind('click', function(event){
+	jQuery('#dataTable').find('tbody').find('tr').bind('click', function(event){
 			if (jQuery(this).hasClass("ui-selected")) {
 				jQuery(this).removeClass("ui-selected");
 			}
 	});
 	//El texto dentro de la grilla no podra seleccionarse
-	jQuery('#form-main\\:dataTable').bind('selectstart', function(event){return false;});
+	jQuery('#dataTable').bind('selectstart', function(event){return false;});
 }
 
 jQuery(document).ready(function(){gridBehavior();});
